443452d6c24bb15193d7353a4b955161c2d1d432
proggrammer <caps.raaj@gmail.com>
2024-03-06 05:17:01 +0530

    first cut

---
+++ src/test/java/org/example/CollatzConjectureTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CollatzConjectureTest {
+
+    @Test
+    void runTest() {
+        CollatzConjecture.runTest(10000);
+    }
+
+    @Test
+    void threeNPlus1() {
+        String s = CollatzConjecture.threeNPlus1("1101");
+        Assertions.assertEquals(s, "101000");
+
+        String s1 = CollatzConjecture.threeNPlus1("1001101");
+        Assertions.assertEquals(s1, "11101000");
+    }
+
+    @Test
+    void collatzStep() {
+        String v = CollatzConjecture.collatzStep("1");
+        Assertions.assertEquals(v, 100);
+    }
+}
