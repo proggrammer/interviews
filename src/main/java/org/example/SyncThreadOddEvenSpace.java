772d6222b09505ceac69d8ff6e3d3e7ada0fcbfd
proggrammer <caps.raaj@gmail.com>
2024-03-06 21:58:08 +0530

    Odd-even-delimiter printing using three separate threads

---
+++ src/main/java/org/example/SyncThreadOddEvenSpace.java
@@ -0,0 +1,75 @@
+package org.example;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class SyncThreadOddEvenSpace {
+
+    private static final Object lock = new Object();
+    private static int counter = 1;
+    private static final char delimiter = ',';
+    private static final int MAX_COUNT = 100;
+
+    private static final String ODD = "odd", EVEN = "even", DEL = "del";
+
+    private static String TURN = ODD;
+
+    public static void print() {
+        System.out.println("Start");
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        List.of(ODD, EVEN, DEL).forEach(name -> executorService.submit(() -> printForEachThread(name)));
+
+        executorService.shutdown();
+        awaitTermination(executorService);
+
+        System.out.println("End");
+    }
+
+    private static void awaitTermination(ExecutorService executorService) {
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void printForEachThread(String threadName) {
+        while (counter <= MAX_COUNT) {
+            synchronized (lock) {
+                if (!threadName.equals(TURN)) {
+                    waitForTurn();
+                } else if (TURN.equals(DEL)) {
+                    printDelimiter();
+                } else {
+                    printNumber();
+                }
+            }
+        }
+    }
+
+    private static void waitForTurn() {
+        try {
+            lock.wait();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void printDelimiter() {
+        System.out.print(delimiter);
+        System.out.flush();
+        TURN = (counter % 2 == 0) ? EVEN : ODD;
+        lock.notifyAll();
+    }
+
+    private static void printNumber() {
+        System.out.print(counter++);
+        System.out.flush();
+        TURN = DEL;
+        lock.notifyAll();
+    }
+}
---
+++ src/test/java/org/example/SyncThreadOddEvenSpaceTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SyncThreadOddEvenSpaceTest {
+
+    @Test
+    void print() {
+        SyncThreadOddEvenSpace.print();
+    }
+}
