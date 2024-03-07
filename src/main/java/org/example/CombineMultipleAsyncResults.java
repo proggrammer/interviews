8c1498c68725c1940159d43d152c65743a6b47f8
proggrammer <caps.raaj@gmail.com>
2024-03-07 17:29:39 +0530

    AsyncTask: join multiple async task to get the final result

---
+++ src/main/java/org/example/CombineMultipleAsyncResults.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CombineMultipleAsyncResults {
+
+    public static void invoke(int n) {
+        // Simulate 50 asynchronous tasks
+        List<CompletableFuture<String>> asyncTasks = createAsyncTasks(n);
+
+        // Combine results when all tasks are completed
+        CompletableFuture<String>[] arrayCF = asyncTasks.toArray(new CompletableFuture[0]);
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(arrayCF);
+
+        // Block and get the final results
+        try {
+            allOf.join(); // Wait for all tasks to complete
+            List<String> results = asyncTasks.stream()
+                    .map(CompletableFuture::join) // Get the result of each completed task
+                    .collect(Collectors.toList());
+
+            // Print or process the combined results
+            results.forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<CompletableFuture<String>> createAsyncTasks(int count) {
+        return IntStream.range(1, count + 1)
+                .mapToObj(i -> {
+                    try {
+                        Thread.sleep(i*1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return CompletableFuture.supplyAsync(() -> "Result from Task " + i);
+                })
+                .collect(Collectors.toList());
+    }
+}
+
---
+++ src/test/java/org/example/CombineMultipleAsyncResultsTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CombineMultipleAsyncResultsTest {
+
+    @Test
+    void invoke() {
+        int n = 10;
+        CombineMultipleAsyncResults.invoke(10);
+    }
+}
